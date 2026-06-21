1ce09e68090885e18bfda7d716c95955388bcbc0
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-21 11:04:46 +0530

    roboshop project

---
+++ 01-mongodb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+ echo "please run the script with root access" | tee -a $LOGS_FOLDER
+ exit 1
+fi
