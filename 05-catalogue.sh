d8e56ce3f02d0beec70b609a10b03e99b75fdadf
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-22 12:29:58 +0530

    working on roboshop proj using shell

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -38,7 +38,7 @@ dnf install nodejs -y &>> LOGS_FILE
 VALIDATE $? "Installing Nodejs:20 server"
 
 id roboshop &>> LOGS_FILE
-if[$? -ne 0] ; then
+if [ $? -ne 0 ]; then
  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
  VALIDATE $? "creating roboshop system user"
 else
